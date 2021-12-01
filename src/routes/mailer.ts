9350447547c113a63d98026197e93a73d76ac9c6
Sergey Sudaev <sudaevs@gmail.com>
2021-12-01 12:47:34 +0300

    add db transaction for forget password flow, add HOST from .env

--- src/routes/mailer.ts
+++ src/routes/mailer.ts
@@ -5,13 +5,16 @@ const mailgun = new Mailgun(formData);
 const key = process.env.MG_KEY || ''
 const domain = process.env.MG_DOMAIN
 export interface Sender {
-    sendEmail: (to: string, subject: string, text: string) => void
+    sendEmail: (to: string, subject: string, text: string) => Promise<unknown>
 }
-const sendEmail = (to: string, subject: string, text: string) => {
+const sendEmail = async (to: string, subject: string, text: string) => {    
     const mg = mailgun.client({ username: 'api', key });
     const data = { from: `"map-nn" <no-reply@${domain}>`, to, subject, text };
-    mg.messages.create(domain, data).catch((error: any) => {
+    try {
+        await mg.messages.create(domain, data, true)
+    } catch(error) {
         console.log(`mail sent to ${to}${error ? ', error ' + error : ''}`)
-    });
+        throw error
+    }
 }
 export default { sendEmail }
