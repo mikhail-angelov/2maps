7918aa1599bb5b0138fd3be28921aa03a6e55c1f
mikhail.angelov <mikhail.angelov@gmail.com>
2022-01-14 13:05:42 +0300

    update map API

--- src/routes/maps.ts
+++ src/routes/maps.ts
@@ -2,28 +2,30 @@ import { CommonRoutesConfig } from './common';
 import express from 'express';
 import _ from 'lodash'
 import * as aws from 'aws-sdk';
-import { Connection } from "typeorm";
+import { Connection, Repository } from "typeorm";
 import { CRequest } from '../../types/express'
 import { Auth } from './auth'
 import { MapFile } from '../entities/mapFile'
+import { MapType, Role } from '../entities/enums'
 
 
 export interface WebMap {
   id: string;
   name: string;
   url: string;
+  type: MapType;
   size: number;
 }
 
-const toWebMap = ({ id, name, url, size }: MapFile): WebMap => ({ id, name, url, size })
+const toWebMap = ({ id, name, url, size, type }: MapFile): WebMap => ({ id, name, url, size, type })
 
 export class Maps implements CommonRoutesConfig {
   auth: Auth
-  db: Connection
+  mapRepo: Repository<MapFile>
   private s3: aws.S3;
   private bucket: string;
   constructor(db: Connection, auth: Auth) {
-    this.db = db;
+    this.mapRepo = db.getRepository(MapFile);
     this.auth = auth;
 
     this.bucket = process.env.S3_BUCKET || '';
@@ -51,11 +53,11 @@ export class Maps implements CommonRoutesConfig {
   }
 
   async refineMapsInDB(maps: Partial<MapFile>[]) {
-    const existingMaps = await this.db.getRepository(MapFile).find()
+    const existingMaps = await this.mapRepo.find()
     const existingMapUrls = existingMaps.map(({ url }) => url)
     const newMaps = maps.filter(({ url }) => url && !existingMapUrls.includes(url))
     if (newMaps.length > 0) {
-      await this.db.getRepository(MapFile).save(newMaps)
+      await this.mapRepo.save(newMaps)
     }
     //todo: delete maps that are not in the list
   }
@@ -65,8 +67,9 @@ export class Maps implements CommonRoutesConfig {
     router.get("", this.auth.authMiddlewareMobile, async (req: CRequest, res: express.Response) => {
       try {
         const user = req.user
-        console.log('load maps', user.email)
-        const maps = await this.db.getRepository(MapFile).find()
+        console.log('load maps for', user.email, user.role)
+        const condition = user.role === Role.user ? {type: MapType.public } : { }
+        const maps = await this.mapRepo.find(condition)
         res.status(200).json(maps.map(toWebMap))
       } catch (e) {
         console.log('load error', e)
@@ -79,7 +82,7 @@ export class Maps implements CommonRoutesConfig {
         const user = req.user
         const { id } = req.params;
         console.log('load map for', user.email, id)
-        const map = await this.db.getRepository(MapFile).findOne(id)
+        const map = await this.mapRepo.findOne(id)
         if (!map) {
           return res.status(400).json({ error: 'invalid request' })
         }
@@ -93,6 +96,24 @@ export class Maps implements CommonRoutesConfig {
         res.status(200).json(toWebMap({ ...map, url: downloadUrl }))
       } catch (e) {
         console.log('load error', e)
+        res.status(400).json({ error: 'invalid request' })
+      }
+    });
+
+    router.put("/:id", this.auth.authAdminMiddleware, async (req: CRequest, res: express.Response) => {
+      try {
+        const user = req.user
+        console.log('update map', req.params.id, req.body)
+        const { name, url, price, size, type } = req.body;
+        await this.mapRepo.update(req.params.id, {name, url, price, size, type });
+        const map = await this.mapRepo.findOne(req.params.id);
+        if(!map) {
+          res.status(400).send("error")
+          return
+        }
+        res.status(200).json(toWebMap(map))
+      } catch (e) {
+        console.log('update error', e)
         res.status(400).json({ error: 'invalid request' })
       }
     });
